cdc99bd4e0f4d6f9d4b1e31a5cd6e1c98c50602f
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2014-10-24 10:19:08 -0700

    .

--- src/0008.rs
+++ src/0008.rs
@@ -2,7 +2,16 @@ fn answer() -> uint {
     0
 }
 
+fn render(s: &str) -> Vec<Vec<uint>> {
+    vec![vec![0]]
+}
+
 #[test]
 fn test() {
     if answer() != 0 { fail!() }
+}
+
+#[test]
+fn test_render() {
+    assert_eq!(render(""), vec![vec![0]]);
 }
