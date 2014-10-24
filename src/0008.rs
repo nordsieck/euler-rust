4e32071621673e3e1594da1f2b269f6171bed354
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2014-10-24 10:41:16 -0700

    .

--- src/0008.rs
+++ src/0008.rs
@@ -2,8 +2,16 @@ fn answer() -> uint {
     0
 }
 
-fn render(s: &str) -> Vec<Vec<uint>> {
-    vec![vec![0]]
+fn render(s: Vec<&str>) -> Vec<Vec<uint>> {
+    let mut v = vec![];
+    for i in range(0, s.len()) {
+        let mut w = vec![];
+        for j in range(0, s[0].len()) {
+            w.push(s[i].char_at(j) as uint - '0' as uint);
+        }
+        v.push(w);
+    }
+    v
 }
 
 #[test]
@@ -13,5 +21,7 @@ fn test() {
 
 #[test]
 fn test_render() {
-    assert_eq!(render(""), vec![vec![0]]);
+    assert_eq!(render(vec![""]), vec![vec![]]);
+    assert_eq!(render(vec!["1"]), vec![vec![1]]);
+    assert_eq!(render(vec!["123","456"]), vec![vec![1, 2, 3], vec![4, 5, 6]]);
 }
