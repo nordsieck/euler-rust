02159ee55016f2ec04e33ec55a06d106d809e8b1
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2015-01-24 14:18:01 -0800

    .

--- src/0008.rs
+++ src/0008.rs
@@ -14,9 +14,19 @@ fn render(s: Vec<&str>) -> Vec<Vec<uint>> {
     v
 }
 
+fn is_connected((ax, ay): (uint, uint), (bx, by): (uint, uint)) -> bool {
+    match ((ax as int - bx as int).abs(), (ay as int - by as int).abs()) {
+        (0, 1) => true,
+        (1, 0) => true,
+        _ => false,
+    }
+}
+
+
+
 #[test]
 fn test() {
-    if answer() != 0 { fail!() }
+    assert_eq!(answer(), 0);
 }
 
 #[test]
@@ -24,4 +34,9 @@ fn test_render() {
     assert_eq!(render(vec![""]), vec![vec![]]);
     assert_eq!(render(vec!["1"]), vec![vec![1]]);
     assert_eq!(render(vec!["123","456"]), vec![vec![1, 2, 3], vec![4, 5, 6]]);
+}
+
+#[test]
+fn test_is_connected() {
+    assert_eq!(is_connected((0, 0), (1, 1)), false);
 }
